5497a727c21bdf5a9e1222737d4eaff9637fb502
kazimanil <kazimanileren@gmail.com>
2018-01-02 19:44:41 +0300

    Introduction of a RMd file.

--- HousePrices_DataMungling.R
+++ HousePrices_DataMungling.R
@@ -1,13 +1,18 @@
 # Data Input ----
-rm(list = ls()); gc(); dev.off()
+rm(list = ls()); gc();
 train <- fread(input = "train.csv", stringsAsFactors = T)
-test  <- fread(input = "test.csv", stringsAsFactors = T)
+test  <- fread(input = "test.csv" , stringsAsFactors = T)
+
+# Train Data Manipulation ----
+# Logarithmic Prices
+train[, logSP := log10(SalePrice)]
 
 # Utilities
-train[Utilities == "AllPub", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 1)]
-train[Utilities == "NoSewr", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 0)]
-train[Utilities == "NoSeWa", ':='(Electricity = 1, Gas = 1, Water = 0, Sewer = 0)]
-train[Utilities == "ELO", ':='(Electricity = 1, Gas = 0, Water = 0, Sewer = 0)]
+train[, Utilities := NULL] # There is no discrepancy among the variable.
+# train[Utilities == "AllPub", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 1)]
+# train[Utilities == "NoSewr", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 0)]
+# train[Utilities == "NoSeWa", ':='(Electricity = 1, Gas = 1, Water = 0, Sewer = 0)]
+# train[Utilities == "ELO", ':='(Electricity = 1, Gas = 0, Water = 0, Sewer = 0)]
 
 # Proximity
 train[, Artery := ifelse(Condition1 == "Artery" | Condition2 == "Artery", 1, 0)]
@@ -21,6 +26,74 @@ train[, RRAe   := ifelse(Condition1 == "RRAe"   | Condition2 == "RRAe", 1, 0)]
 train[, Norm   := ifelse(Artery + Feedr + RRNn + RRAn + PosN + PosA + RRNe + RRAe < 1, 1, 0)]
 
 # Exterior
+train[, Exterior1st := as.character(Exterior1st)]
+train[, Exterior2nd := as.character(Exterior2nd)]
 train[Exterior2nd == "Brk Cmn", Exterior2nd := "BrkComm"]
 train[Exterior2nd == "CmentBd", Exterior2nd := "CemntBd"]
 train[Exterior2nd == "Wd Shng", Exterior2nd := "WdShing"]
+train[, DoubleExterior := ifelse(Exterior1st != Exterior2nd, 1, 0)]
+train[, Shingles := ifelse(Exterior1st %in% c("AsbShng", "AsphShn", "WdShing") | Exterior2nd %in% c("AsbShng", "AsphShn", "WdShing"), 1, 0)]
+train[, Brick    := ifelse(Exterior1st %in% c("BrkComm", "BrkFace", "CBlock", "Stone") | Exterior2nd %in% c("BrkComm", "BrkFace", "CBlock", "Stone"), 1, 0)]
+train[, Board    := ifelse(Exterior1st %in% c("CemntBd", "HdBoard", "Plywood") | Exterior2nd %in% c("CemntBd", "HdBoard", "Plywood"), 1, 0)]
+train[, Stucco   := ifelse(Exterior1st %in% c("ImStucc", "Stucco") | Exterior2nd %in% c("ImStucc", "Stucco"), 1, 0)]
+train[, Siding   := ifelse(Exterior1st %in% c("MetalSd", "VinylSd", "Wd Sdng") | Exterior2nd %in% c("MetalSd", "VinylSd", "Wd Sdng"), 1, 0)]
+train[, Other    := ifelse(Exterior1st %in% c("Other", "PreCast") | Exterior2nd %in% c("Other", "PreCast"), 1, 0)]
+train[, Exterior1st := as.factor(Exterior1st)]
+train[, Exterior2nd := as.factor(Exterior2nd)]
+
+# Dwelling Classes
+train[, MSSubClass := as.factor(MSSubClass)]
+train[MSSubClass %in% c("30", "70"), Age := as.factor("Old")] 
+train[MSSubClass %in% c("20", "60", "120", "160"), Age := as.factor("New")]
+train[MSSubClass %in% c("40", "45", "50", "75", "90", "150", "190"), Age := as.factor("All")]
+train[, PUD := as.factor(ifelse(MSSubClass %in% c("120", "150", "160", "180"), 1, 0))]
+train[MSSubClass %in% c("20", "30", "40", "120"),       Story := as.factor("1")]
+train[MSSubClass %in% c("45", "50", "150"),             Story := as.factor("1-1/2")]
+train[MSSubClass %in% c("60", "70", "85", "90", "160"), Story := as.factor("2")]
+train[MSSubClass %in% c("75", "80", "180", "190"),      Story := as.factor("MULTI")]
+
+# Test Data Manipulation ----
+# Utilities
+test[, Utilities := NULL] # There is no discrepancy among the variable.
+# test[Utilities == "AllPub", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 1)]
+# test[Utilities == "NoSewr", ':='(Electricity = 1, Gas = 1, Water = 1, Sewer = 0)]
+# test[Utilities == "NoSeWa", ':='(Electricity = 1, Gas = 1, Water = 0, Sewer = 0)]
+# test[Utilities == "ELO", ':='(Electricity = 1, Gas = 0, Water = 0, Sewer = 0)]
+
+# Proximity
+test[, Artery := ifelse(Condition1 == "Artery" | Condition2 == "Artery", 1, 0)]
+test[, Feedr  := ifelse(Condition1 == "Feedr"  | Condition2 == "Feedr", 1, 0)]
+test[, RRNn   := ifelse(Condition1 == "RRNn"   | Condition2 == "RRNn", 1, 0)]
+test[, RRAn   := ifelse(Condition1 == "RRAn"   | Condition2 == "RRAn", 1, 0)]
+test[, PosN   := ifelse(Condition1 == "PosN"   | Condition2 == "PosN", 1, 0)]
+test[, PosA   := ifelse(Condition1 == "PosA"   | Condition2 == "PosA", 1, 0)]
+test[, RRNe   := ifelse(Condition1 == "RRNe"   | Condition2 == "RRNe", 1, 0)]
+test[, RRAe   := ifelse(Condition1 == "RRAe"   | Condition2 == "RRAe", 1, 0)]
+test[, Norm   := ifelse(Artery + Feedr + RRNn + RRAn + PosN + PosA + RRNe + RRAe < 1, 1, 0)]
+
+# Exterior
+test[, Exterior1st := as.character(Exterior1st)]
+test[, Exterior2nd := as.character(Exterior2nd)]
+test[Exterior2nd == "Brk Cmn", Exterior2nd := "BrkComm"]
+test[Exterior2nd == "CmentBd", Exterior2nd := "CemntBd"]
+test[Exterior2nd == "Wd Shng", Exterior2nd := "WdShing"]
+test[, DoubleExterior := ifelse(Exterior1st != Exterior2nd, 1, 0)]
+test[, Shingles := ifelse(Exterior1st %in% c("AsbShng", "AsphShn", "WdShing") | Exterior2nd %in% c("AsbShng", "AsphShn", "WdShing"), 1, 0)]
+test[, Brick    := ifelse(Exterior1st %in% c("BrkComm", "BrkFace", "CBlock", "Stone") | Exterior2nd %in% c("BrkComm", "BrkFace", "CBlock", "Stone"), 1, 0)]
+test[, Board    := ifelse(Exterior1st %in% c("CemntBd", "HdBoard", "Plywood") | Exterior2nd %in% c("CemntBd", "HdBoard", "Plywood"), 1, 0)]
+test[, Stucco   := ifelse(Exterior1st %in% c("ImStucc", "Stucco") | Exterior2nd %in% c("ImStucc", "Stucco"), 1, 0)]
+test[, Siding   := ifelse(Exterior1st %in% c("MetalSd", "VinylSd", "Wd Sdng") | Exterior2nd %in% c("MetalSd", "VinylSd", "Wd Sdng"), 1, 0)]
+test[, Other    := ifelse(Exterior1st %in% c("Other", "PreCast") | Exterior2nd %in% c("Other", "PreCast"), 1, 0)]
+test[, Exterior1st := as.factor(Exterior1st)]
+test[, Exterior2nd := as.factor(Exterior2nd)]
+
+# Dwelling Classes
+test[, MSSubClass := as.factor(MSSubClass)]
+test[MSSubClass %in% c("30", "70"), Age := as.factor("Old")] 
+test[MSSubClass %in% c("20", "60", "120", "160"), Age := as.factor("New")]
+test[MSSubClass %in% c("40", "45", "50", "75", "90", "150", "190"), Age := as.factor("All")] 
+test[, PUD     := as.factor(ifelse(MSSubClass %in% c("120", "150", "160", "180"), 1, 0))]
+test[MSSubClass %in% c("20", "30", "40", "120"),       Story := as.factor("1")]
+test[MSSubClass %in% c("45", "50", "150"),             Story := as.factor("1-1/2")]
+test[MSSubClass %in% c("60", "70", "85", "90", "160"), Story := as.factor("2")]
+test[MSSubClass %in% c("75", "80", "180", "190"),      Story := as.factor("MULTI")]
