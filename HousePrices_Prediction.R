4cd722eb3b98c507907b2aabbbc8799068cb7e3f
kazimanil <kazimanileren@gmail.com>
2018-01-09 17:44:28 +0300

    XG Boost predictions submitted.

--- HousePrices_Prediction.R
+++ HousePrices_Prediction.R
@@ -4,6 +4,7 @@ load("HousePrices_AfterEDA.RData")
 library(randomForest)
 library(inTrees)
 library(xgboost)
+library(Matrix)
 
 # Last Minute Call for Data Mungling ----
 train <- cbind(train[, c("Id", "SalePrice", "logSP")],
@@ -23,7 +24,7 @@ test[is.na(SaleType), SaleType := "Oth"]
 x_test <- test[, 3:51]
 x_test <- x_test[, -c("HouseStyle", "Exterior1st", "Exterior2nd")] # different factor levels
 
-# Random Forest ----
+# Random Forest %14.65 RMSE ----
 rf1000 <- randomForest(x = x_train, y = y_train,
                        xtest = x_test,
                        ntree = 1000, importance = TRUE)
@@ -48,3 +49,12 @@ rf_prunepred <- cbind(test[, c("Id", "MiscVal")],
                      logSP_pred = rf_pruned$test$predicted)
 rf_prunepred <- rf_prunepred[, .(Id, SalePrice = 10^logSP_pred + MiscVal)]
 fwrite(rf_prunepred, file = "Submission_HousePrices_RandomForest_Pruned.csv")
+
+# XG Boost %18,36 RMSE ----
+xg_model <- xgboost(data = data.matrix(x_train), label = y_train,
+                    silent = 1, booster = "gblinear", nrounds = 25)
+ypred <- predict(xg_model, data.matrix(x_test))
+xgb <- cbind(test[, c("Id", "MiscVal")],
+             logSP_pred = ypred)
+xgb <- xgb[, .(Id, SalePrice = 10^logSP_pred + MiscVal)]
+fwrite(xgb, file = "Submission_HousePrices_XGBoost.csv")
